9daa7231ece3dfe5f2cecc8ab47bfe77a316305f
jakeowens93 <80400379+jakeowens93@users.noreply.github.com>
2021-06-24 11:17:04 -0500

    add phrases and getRandomPhraseAsArray function

--- app.js
+++ app.js
@@ -2,9 +2,32 @@ const querty = document.getElementById('querty');
 const phrase = document.getElementById('phrase');
 let missed = 0; 
 const btnReset = document.querySelector('.btn__reset');
+const phrases = [
+    "phrase 1",
+    "phrase 2",
+    "phrase 3",
+    "phrase 4",
+    "phrase 5"
+]
 
+// ======== Start Game Button ========= //
 
 btnReset.addEventListener('click', () => {
     const overlay = document.getElementById('overlay');
     overlay.style.display = 'none';
 });
+
+// ======== Random Phrase Function ========= //
+function getRandomPhraseAsArray (arr) {
+    var randomNumber = Math.floor(Math.random()*phrases.length);
+    var randomPhrase = phrases[randomNumber];
+    var splitPhrase = randomPhrase.split('');
+    return splitPhrase
+}
+const phraseArray = getRandomPhraseAsArray(phrases);
+
+// ======== Set the Game Display ========= //
+
+function addPhraseToDisplay(arr){
+    // do stuff any arr that is passed in, and add to `#phrase ul`
+}
