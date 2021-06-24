7e24270e72e9b9402ea098cd38a3c788d32abeb8
jakeowens93 <80400379+jakeowens93@users.noreply.github.com>
2021-06-24 12:52:12 -0500

    add the addPhraseToDisplay function

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const phrase = document.getElementById('phrase');
 let missed = 0; 
 const btnReset = document.querySelector('.btn__reset');
 const phrases = [
-    "phrase 1",
-    "phrase 2",
-    "phrase 3",
-    "phrase 4",
-    "phrase 5"
+    "hello bitch",
+    "you had all summer to figure out what you do",
+    "bart harley jarvis",
+    "hot topic pastor",
+    "newt gingrich eating chili"
 ]
 
 // ======== Start Game Button ========= //
@@ -24,10 +24,27 @@ function getRandomPhraseAsArray (arr) {
     var splitPhrase = randomPhrase.split('');
     return splitPhrase
 }
+
 const phraseArray = getRandomPhraseAsArray(phrases);
 
 // ======== Set the Game Display ========= //
-
-function addPhraseToDisplay(arr){
+function addPhraseToDisplay(){
     // do stuff any arr that is passed in, and add to `#phrase ul`
+    for (var i = 0; i < phraseArray.length; i++){
+        var li = document.createElement('li');
+        var text = document.createTextNode(phraseArray[i]);
+        li.appendChild(text);
+        document.getElementById("phrase").appendChild(li);
+        
+        // phraseArray[i];
+        // const ul = document.querySelector('#phrase ul');
+        // list = document.createElement('li');
+        // list.appendChild(text);
+        // ul.appendChild(list);
+        console.log(li);
+    }
 }
+addPhraseToDisplay(phraseArray);
+
+// ======== Letter Checker Function ======== //
+// function checkLetter()
